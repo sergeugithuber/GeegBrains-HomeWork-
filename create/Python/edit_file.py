986c966159307e24fd36c0225b6df2e04b433a47
sergeugithuber <github_lsplz@mail.ru>
2022-10-11 19:41:46 +0300

    Дз к первому семинару

---
+++ create/Python/edit_file.py
@@ -0,0 +1,50 @@
+# from array import array
+
+
+# def Proverka(a, b):
+#    if a**2 == b or b**2 == a:
+#       return "yes"
+#    else:
+#       return "no"
+# a = int(input("1 число = "))
+# b = int(input('2 число = '))
+# print(Proverka(a, b))
+
+# from macpath import split
+
+
+# array = [10, 2, 4, 55, 31]
+# def Find(array):
+#    max = 0
+#    for i in array:
+#       if i > max:
+#          max = i
+#    return max
+# print(Find(array))
+# ben = list(map(int, input(), split()))
+
+# N = int(input("Введите число: "))
+# reN = -N
+# while reN <= N:
+#    print(reN, end= " ")
+#    reN += 1
+
+# number = input('Введите дробь: ')
+# print(number[2])
+
+# duration = int(input("Введите кол-во времени в секундах: "))
+# m = duration // 60
+# duration %= 60
+# h = m // 60
+# m %= 60
+# d = h // 24
+# h %= 24
+# print(d, "day")
+# print(h, "hour")
+# print(m, "min")
+# print(duration, "sec")
+
+from datetime import datetime 
+now = datetime.now() 
+current_time = now.strftime("%H:%M:%S") 
+print("Current Time =", current_time)
