76a48e727346d0b16273fb8d1da282780f355a06
sergeugithuber <github_lsplz@mail.ru>
2022-11-14 17:59:02 +0300

    ДЗ к 6-ому семинару

--- create/Program.cs
+++ create/Program.cs
@@ -97,36 +97,38 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
 
-Console.Write("Введите кол-во строк: "); int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кол-во столбцов: "); int n = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = new int[m, n];
-FillArray(matrix);
+// Console.Write("Введите кол-во строк: "); int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: "); int n = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = new int[m, n];
+// FillArray(matrix);
 
-void FillArray(int[,] matrix)
-{
-   for (int i = 0; i < matrix.GetLength(0); i++)
-   {
-      for (int j = 0; j < matrix.GetLength(1); j++)
-      {
-         matrix[i, j] = new Random().Next(0, 10);
+// void FillArray(int[,] matrix)
+// {
+//    for (int i = 0; i < matrix.GetLength(0); i++)
+//    {
+//       for (int j = 0; j < matrix.GetLength(1); j++)
+//       {
+//          matrix[i, j] = new Random().Next(0, 10);
 
-         Console.Write($"{matrix[i, j]} ");
-      }
-      Console.WriteLine();
-   }
-}
-Console.WriteLine();
-int sum = 0;
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-   for (int j = 0; j < matrix.GetLength(1); j++)
-   {
-      sum = sum + matrix[i, j];
-      if (j == matrix.GetLength(0))
-      {
-         double sqrt = Convert.ToDouble(sum) / matrix.GetLength(1);
-         Console.WriteLine($"Среднее значение {i + 1} столбца = {sqrt:F3}");
-         sum = 0;
-      }
-   }
-}
+//          Console.Write($"{matrix[i, j]} ");
+//       }
+//       Console.WriteLine();
+//    }
+// }
+// Console.WriteLine();
+// int sum = 0;
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//    for (int j = 0; j < matrix.GetLength(1); j++)
+//    {
+//       sum = sum + matrix[i, j];
+//       if (j == matrix.GetLength(0))
+//       {
+//          double sqrt = Convert.ToDouble(sum) / matrix.GetLength(1);
+//          Console.WriteLine($"Среднее значение {i + 1} столбца = {sqrt:F3}");
+//          sum = 0;
+//       }
+//    }
+// }
+
+Console.WriteLine("Всё окей!");
